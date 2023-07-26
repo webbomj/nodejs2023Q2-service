61ad62727ab3c0548e29d117ae86bac4fb156700
webbomj <nethack@mail.ru>
2023-07-26 19:18:08 +0300

    feat: add db

---
+++ src/api/user/user.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { DbService } from 'src/db/db/db.service';
+
+@Injectable()
+export class UserService {
+  constructor(private db: DbService) {}
+
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
+
+  findAll() {
+    return this.db.users;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApiModule } from './api/api.module';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [],
+  imports: [ApiModule, DbModule],
   controllers: [AppController],
   providers: [AppService],
 })
