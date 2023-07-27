3f9e1df1c9dfc9dc1ca898575813ea4bebe9a78d
webbomj <nethack@mail.ru>
2023-07-27 20:06:04 +0300

    feat: edit OpenApi, add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
+import { load } from 'js-yaml';
+import * as fs from 'node:fs/promises';
+import * as path from 'node:path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const dockFilePath = path.join(__dirname, '../', '/doc', 'api.yaml');
+  const doc = await fs.readFile(dockFilePath, 'utf8');
+
+  SwaggerModule.setup('', app, load(doc) as OpenAPIObject);
+
   await app.listen(4000);
 }
 bootstrap();
