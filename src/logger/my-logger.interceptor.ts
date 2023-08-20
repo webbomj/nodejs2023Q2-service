ad248589a5f8788d3ff9fdacaee9cfabc4fcfb9c
webbomj <nethack@mail.ru>
2023-08-20 09:42:55 +0300

    fix: request to response

--- src/logger/my-logger.interceptor.ts
+++ src/logger/my-logger.interceptor.ts
@@ -7,6 +7,7 @@ import {
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { MyLoggerService } from './logger.service';
+import { Request, Response } from 'express';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
@@ -14,15 +15,15 @@ export class LoggingInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const now = Date.now();
     const ctx = context.switchToHttp();
-    const request = ctx.getRequest();
-    const response = ctx.getRequest();
+    const request = ctx.getRequest<Request>();
+    const response = ctx.getResponse<Response>();
 
     return next.handle().pipe(
       tap(() => {
         const message = `[${request.url}], query: ${JSON.stringify(
           request.query,
         )}, body: ${JSON.stringify(request.body)}, code: ${
-          response.res.statusCode
+          response.statusCode
         } ${Date.now() - now}ms`;
         return this.logger.log(message);
       }),
